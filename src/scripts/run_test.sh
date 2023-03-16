b9e26f7de91cffe7b7d475c5ab7ea3b3d21cb80c
Oscar Santos <oscarsantosmu@gmail.com>
2023-03-15 18:39:39 -0600

    feat: make script executable from local env & VPS

--- src/scripts/run_test.sh
+++ src/scripts/run_test.sh
@@ -1,4 +1,12 @@
 #!/bin/env bash
 
+if [ "$1" == "Windows" ]; then
+    # Windows
+    VENV_PATH="python3-virtualenv/Scripts/python.exe"
+else
+    # Linux (default)
+    VENV_PATH="python3-virtualenv/bin/python"
+fi
+
 cd "${PROJECTPATH}" || exit
-python3-virtualenv/bin/python -m unittest discover -v tests/
+"${VENV_PATH}" -m unittest discover -v tests/
