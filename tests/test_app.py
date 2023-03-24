ced2674d75a17bf0a34eee37f2298ee8b25c2a61
Oscar Santos <oscarsantosmu@gmail.com>
2023-03-24 02:21:00 -0600

    test: update test_timeline with mocks
    This feature will avoid network traffic in the tests
    
    Since timeline do requests to API it had a lot of troubles in the tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
-import unittest
 import os
+import unittest
+import responses
+from unittest.mock import patch, MagicMock
+
+from flask import url_for
 
 os.environ["TESTING"] = "true"
 
@@ -18,7 +22,9 @@ class AppTestCase(unittest.TestCase):
         assert '<a class="nav-link" href="#about">About</a>' in html
         assert '<span class="pull"> Pull </span>' in html
 
-    def test_timeline(self):
+    @patch("requests.post")
+    @patch("requests.get")
+    def test_timeline(self, mock_get, mock_post):
         response = self.client.get("/api/timeline_post", follow_redirects=True)
         assert response.status_code == 200
         assert response.is_json
@@ -52,11 +58,38 @@ class AppTestCase(unittest.TestCase):
         assert json["timeline_posts"][0]["name"] == "John Doe"
 
         # TODO Add more tests relating to the timeline page
+        # Set up mocked responses for GET
+        mock_get.return_value.ok = True
+        mock_get.return_value.json.return_value = {
+            "timeline_posts": [
+                {"name": "John", "email": "john@example.com", "content": "Post 1"}
+            ]
+        }
         response = self.client.get("/timeline")
+        # Check that expected data is displayed on page
         assert response.status_code == 200
         html = response.get_data(as_text=True)
         assert '<div class="col-md-12">' in html
         assert "<h2>Timeline Posts</h2>" in html
+        self.assertIn(b"Post 1", response.data)
+
+        # Set up mocked responses for POST when it succeeds
+        mock_post.return_value.ok = True
+        data = {"name": "Jane", "email": "jane@example.com", "content": "Post 2"}
+        response = self.client.post("/timeline", data=data)
+        # Check that expected data is displayed on page after the POST
+        assert response.status_code == 200
+        self.assertIn("Your post has been created", response.get_data(as_text=True))
+
+        # Set up mocked responses for POST when it fails
+        mock_post.return_value.ok = False
+        data = {"name": "Jane", "email": "not-an-email", "content": "Post 2"}
+        response = self.client.post("/timeline", data=data)
+        # Check that expected data is displayed on page after the POST
+        assert response.status_code == 200
+        self.assertIn(
+            "There was an error submitting your post", response.get_data(as_text=True)
+        )
 
     def test_malformed_timeline_post(self):
         response = self.client.post(
